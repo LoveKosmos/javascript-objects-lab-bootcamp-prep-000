cc0a7c04560ec0e7fe2fa7f78f26b4f5da1e6ddc
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 23:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function destructivelyDeleteFromObjectByKey(object,key){
-  delete object.key;
+  delete object[key];
   return object
   
 }
