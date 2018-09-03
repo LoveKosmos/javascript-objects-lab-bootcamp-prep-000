dbe7f6a404c37a04551f97482b8ac7bf068beffb
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 23:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey( object,key){
    var newObj = Object.assign({},key)
-   delete newObj.key
+   delete newObj[key];
    return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
