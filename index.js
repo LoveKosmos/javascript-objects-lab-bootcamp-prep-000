ee94d257a753dede67fda36b7a3b713ef5f9a9df
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 23:42:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
   object[key]=value;
   const recipes={};
-  object
-  return Object.assign({},key, value)
+  return object
 }
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  const recipes={};
+  object
+  return 
+  Object.assign({},key, value)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object
