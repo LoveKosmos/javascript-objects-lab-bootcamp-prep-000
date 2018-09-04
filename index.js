c76707cd3f94e9fa332f49ab70d97351912a636b
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-04 00:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function updateObjectWithKeyAndValue(object, key, value){
-   return Object.assign({},key, value)
+   return Object.assign({},object, {[key] : value})
  
 }
 function deleteFromObjectByKey( object,key){
